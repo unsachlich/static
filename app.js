a3015973484c07198e8e5fff1163985bed3e5d05
Bernd Schiel <bernd.schiel@gmail.com>
2017-03-09 00:56:56 +0100

    Complete little Fileserver.

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-var http = require('http');
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('combined'));
+
+var staticPath = path.join(__dirname, 'static');
+app.use(express.static(staticPath));
+
+app.use(function(request, response) {
+  response.status(404);
+  response.send('Datei nicht gefunden!');
+});
+
+app.use(function(error, request, response, next) {
+  console.error(error);
+  next(error);
+});
+
+app.use(function(error, request, response, next) {
+  response.status(500);
+  response.send('Interner Serverfehler!');
+});
+
+app.listen(3000, function() {
+	console.log('App started on port 3000');
+});
